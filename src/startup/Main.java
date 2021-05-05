a51d7ecf36da1587382a7c028f3da883cd1386b7
Dennis Hadzialic <dennis.hadzialic@hotmail.com>
2021-05-05 20:06:36 +0200

    Added View-class

--- src/startup/Main.java
+++ src/startup/Main.java
@@ -1,4 +1,12 @@
 package startup;
 
 public class Main {
+    /**
+     * The main function of the program
+     *
+     * @param args no terminal arguments/parameters
+     */
+    public static void main(String[] args) {
+
+    }
 }
