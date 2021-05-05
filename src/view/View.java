a59ea8d5aa32d031cd29f1ee08a5dab9157a03ee
Dennis Hadzialic <dennis.hadzialic@hotmail.com>
2021-05-05 20:20:49 +0200

    Added contructor in View

--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,20 @@
 package view;
+import controller.Controller;
 
+/**
+ * The class which the user should interact with
+ *
+ * @author dennishadzialic
+ */
 public class View {
+    private final Controller controller;
+
+    /**
+     * Constructor for class View
+     *
+     * @param controller a reference to the controller created in Main.java
+     */
+    public View(Controller controller) {
+        this.controller = controller;
+    }
 }
