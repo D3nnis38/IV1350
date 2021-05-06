4dd3921a3493c772c94880e0495c333a86606259
Dennis Hadzialic <dennis.hadzialic@hotmail.com>
2021-05-06 14:06:06 +0200

    Added all methods in ProductDTO

---
+++ src/model/DTO/ProductDTO.java
@@ -0,0 +1,66 @@
+package model.DTO;
+
+/**
+ * Respresents a Data Transfer Object of the class Product
+ *
+ * @author dennishadzialic
+ */
+public class ProductDTO {
+    private final String itemIdentifier;
+    private final double price;
+    private final String itemDescription;
+    private final float VAT;
+    private final int soldQuantity;
+
+    /**
+     * Constructor for ProductDTO
+     *
+     * @param itemIdentifier  identifies the item with a uniqe comibantion of numbers
+     * @param price           is the set price of the product
+     * @param itemDescription is a description of the product
+     * @param VAT             is the VAT rate based on the product
+     * @param soldQuantity    is the quantaty sold of the product
+     */
+    public ProductDTO(String itemIdentifier, double price, String itemDescription, float VAT, int soldQuantity) {
+        this.itemIdentifier = itemIdentifier;
+        this.price = price;
+        this.itemDescription = itemDescription;
+        this.VAT = VAT;
+        this.soldQuantity = soldQuantity;
+    }
+
+    /**
+     * @return the unique itemIdentifier
+     */
+    public String getItemIdentifier() {
+        return itemIdentifier;
+    }
+
+    /**
+     * @return the specific price of the product
+     */
+    public double getPrice() {
+        return price;
+    }
+
+    /**
+     * @return the itemDescription
+     */
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    /**
+     * @return the VAT-rate for the item
+     */
+    public float getVAT() {
+        return VAT;
+    }
+
+    /**
+     * @return the qunatity sold
+     */
+    public int getSoldQuantity() {
+        return soldQuantity;
+    }
+}
